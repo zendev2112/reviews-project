6865eb4f919e619a13af33acbe1f090d1bb54f74
zen <zenon.deviagge@gmail.com>
2024-03-06 17:22:58 -0300

    fix previous and next buttons of the list

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,29 @@ import {
 const App = () => {
   const [index, setIndex] = useState(0)
   const { name, job, image, text } = people[index]
-  
+
+  const checkNumber = (number) => {
+    if (newIndex > people.length - 1) {
+      return 0
+    }
+    if (newIndex < 0) {
+      return people.length - 1
+    }
+    return number
+  }
+
   const nextPerson = () => {
     setIndex((currentIndex) => {
-      const newIndex = currentIndex + 1
-      return newIndex
+      const newIndex = currentIndex + 1;
+      return checkNumber(newIndex)
     })
   }
   const prevPerson = () => {
     setIndex((currentIndex) => {
-      const newIndex = currentIndex - 1
-      return newIndex
+      const newIndex = currentIndex - 1;
+      return checkNumber(newIndex)
     })
   }
-  
 
   return (
     <main>
@@ -31,16 +40,16 @@ const App = () => {
           <img src={image} alt={name} className="person-img" />
           <span className="quote-icon">
             <FaQuoteRight />
-          </span>          
+          </span>
         </div>
-        <h4 className='author'>{name}</h4>
-        <p className='job'>{job}</p>
-        <p className='info'>{text}</p>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
         <div className="btn-container">
-          <button className='prev-btn' onClick={prevPerson}>
+          <button className="prev-btn" onClick={prevPerson}>
             <FaChevronCircleLeft />
           </button>
-          <button className='next-btn' onClick={nextPerson}>
+          <button className="next-btn" onClick={nextPerson}>
             <FaChevronCircleRight />
           </button>
         </div>
