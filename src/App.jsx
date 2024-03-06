69592097732631b136cfd4175bd32030fd91f1a0
zen <zenon.deviagge@gmail.com>
2024-03-06 16:41:08 -0300

    display main component with image, name, job and text

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,29 @@
+import { useState } from 'react'
+import people from './data'
+import {
+  FaChevronCircleLeft,
+  FaChevronCircleRight,
+  FaQuoteRight,
+} from 'react-icons/fa'
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
-};
-export default App;
+  const [index, setIndex] = useState(0)
+  const { name, job, image, text } = people[index]
+  console.log(name)
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>          
+        </div>
+        <h4 className='author'>{name}</h4>
+        <p className='job'>{job}</p>
+        <p className='info'>{text}</p>
+      </article>
+    </main>
+  )
+}
+export default App
