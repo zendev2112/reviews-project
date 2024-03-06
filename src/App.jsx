3f679bfcbd5fca4f1f5e6c1241947e4fc4abf5bc
zen <zenon.deviagge@gmail.com>
2024-03-06 17:04:30 -0300

    add preview and next buttons for the search, still index limit bug must be solved

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,21 @@ import {
 const App = () => {
   const [index, setIndex] = useState(0)
   const { name, job, image, text } = people[index]
-  console.log(name)
+  
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1
+      return newIndex
+    })
+  }
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1
+      return newIndex
+    })
+  }
+  
+
   return (
     <main>
       <article className="review">
@@ -22,6 +36,14 @@ const App = () => {
         <h4 className='author'>{name}</h4>
         <p className='job'>{job}</p>
         <p className='info'>{text}</p>
+        <div className="btn-container">
+          <button className='prev-btn' onClick={prevPerson}>
+            <FaChevronCircleLeft />
+          </button>
+          <button className='next-btn' onClick={nextPerson}>
+            <FaChevronCircleRight />
+          </button>
+        </div>
       </article>
     </main>
   )
